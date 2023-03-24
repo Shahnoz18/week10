45e6fc8dd4b457856e2df96484ad6f74049b506c
Shahnoz18 <shob181296@mail.ru>
2023-03-24 16:26:24 +0300

    text

---
+++ assets/script/index.js
@@ -0,0 +1,17 @@
+function showMassage() 
+{
+    alert('"Я учу JavaScript!"');
+}
+showMassage ();
+
+const blockFoto = document.getElementById("photo");
+
+function leftShowPhoto() {
+    blockFoto.style.backgroundImage =
+     "url('https://images11.graziamagazine.ru/upload/img_cache/6f3/6f3aaa02620ee2b8baf302eebda2a55e_cropped_666x666.jpeg')"
+}
+function rightShowPhoto() {
+    blockFoto.style.backgroundImage =
+     "url('https://oir.mobi/uploads/posts/2021-04/1619619348_59-oir_mobi-p-samie-milie-kotiki-zhivotnie-krasivo-foto-65.jpg')"
+}
+    
